abd1766ce1d40ea1b85217e0e00d836d7de3590c
Antonio Mallia <me@antoniomallia.it>
2020-03-25 19:52:21 +0100

    Added if-else

--- engines/pisa-0.8.0/tools/do_query.cpp
+++ engines/pisa-0.8.0/tools/do_query.cpp
@@ -3,19 +3,34 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 #include <boost/algorithm/string.hpp>
+#include <query/queries.hpp>
+#include <query/term_processor.hpp>
 
 int main(int argc, char const *argv[])
 {
     spdlog::drop("");
     spdlog::set_default_logger(spdlog::stderr_color_mt(""));
 
+    std::string terms_file;
+    std::string stemmer = "porter2";
+
+    auto term_processor = pisa::TermProcessor(terms_file, std::nullopt, stemmer);
     std::string line;
     while (std::getline(std::cin, line))
     {
+        size_t count = 0;
         std::vector<std::string> tokens;
         boost::split(tokens, line, boost::is_any_of("\t"));
+        pisa::Query q = pisa::parse_query_terms(tokens[1], term_processor);
+        if(tokens[0] == "COUNT"){
 
-        std::cout << "UNSUPPORTED" << std::endl;
+        } else if(tokens[0] == "TOP_10"){
+        } else if(tokens[0] == "TOP_10_COUNT"){
+
+        }else{
+            std::cout << "UNSUPPORTED\n";
+        }
+        std::cout << count << "\n";
     }
 
 }
