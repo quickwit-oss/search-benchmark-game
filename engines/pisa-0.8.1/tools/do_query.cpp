c5c2180369bc297160c2a3a65ec03098fdb2ae3e
Antonio Mallia <me@antoniomallia.it>
2020-03-25 22:41:28 +0100

    Finished query tool

--- engines/pisa-0.8.1/tools/do_query.cpp
+++ engines/pisa-0.8.1/tools/do_query.cpp
@@ -1,43 +1,108 @@
 #include <iostream>
 
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/spdlog.h>
 #include <boost/algorithm/string.hpp>
+#include <cursor/block_max_scored_cursor.hpp>
+#include <cursor/cursor.hpp>
+#include <cursor/scored_cursor.hpp>
+#include <index_types.hpp>
+#include <mio/mmap.hpp>
+#include <query/algorithm.hpp>
 #include <query/queries.hpp>
 #include <query/term_processor.hpp>
+#include <scorer/scorer.hpp>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+#include <topk_queue.hpp>
+#include <wand_data.hpp>
+#include <wand_data_compressed.hpp>
 
-int main(int argc, char const *argv[])
-{
-    spdlog::drop("");
-    spdlog::set_default_logger(spdlog::stderr_color_mt(""));
+int main(int argc, char const *argv[]) {
+  using namespace pisa;
+  spdlog::drop("");
+  spdlog::set_default_logger(spdlog::stderr_color_mt(""));
 
-    std::string terms_file;
-    std::string stemmer = "porter2";
+  std::string terms_file;
+  std::string wand_data_filename;
+  std::string index_filename;
 
-    // auto term_processor = pisa::TermProcessor(terms_file, std::nullopt, stemmer);
-    std::string line;
-    while (std::getline(std::cin, line))
-    {
-        size_t count = 0;
-        std::vector<std::string> tokens;
-        boost::split(tokens, line, boost::is_any_of("\t"));
-        if(boost::starts_with(tokens[1], "\"")){
-            std::cout << "UNSUPPORTED\n";
-            continue;
-        } else if(boost::starts_with(tokens[1], "+")){
-            // AND Query
-        }
+  std::string stemmer = "porter2";
+  std::string scorer_name = "bm25";
+  size_t k = 10;
+  bool intersection = false;
 
-        // pisa::Query q = pisa::parse_query_terms(tokens[1], term_processor);
-        if(tokens[0] == "COUNT"){
+  auto term_processor = TermProcessor(terms_file, std::nullopt, stemmer);
+  using wand_uniform_index_quantized =
+      wand_data<wand_data_compressed<PayloadType::Quantized>>;
+  wand_uniform_index_quantized wdata;
 
-        } else if(tokens[0] == "TOP_10"){
-        } else if(tokens[0] == "TOP_10_COUNT"){
+  mio::mmap_source md;
+  std::error_code error;
+  md.map(wand_data_filename, error);
+  if (error) {
+    std::cerr << "error mapping file: " << error.message() << ", exiting..."
+              << std::endl;
+    throw std::runtime_error("Error opening file");
+  }
+  mapper::map(wdata, md, mapper::map_flags::warmup);
 
-        }else{
-            std::cout << "UNSUPPORTED\n";
-        }
-        std::cout << count << "\n";
+  using IndexType = block_simdbp_index;
+
+  IndexType index;
+  spdlog::info("Loading index from {}", index_filename);
+  mio::mmap_source m(index_filename.c_str());
+  mapper::map(index, m);
+
+  auto scorer = scorer::from_name(scorer_name, wdata);
+
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    size_t count = 0;
+    std::vector<std::string> tokens;
+    boost::split(tokens, line, boost::is_any_of("\t"));
+    if (boost::starts_with(tokens[1], "\"")) {
+      std::cout << "UNSUPPORTED\n";
+      continue;
+    } else if (boost::starts_with(tokens[1], "+")) {
+      intersection = true;
+      boost::replace_all(tokens[1], "+", "");
     }
 
+    topk_queue topk(k);
+    Query query = parse_query_terms(tokens[1], term_processor);
+    if (tokens[0] == "COUNT") {
+      if (intersection) {
+        and_query and_q;
+        count = and_q(make_cursors(index, query), index.num_docs()).size();
+      } else {
+        or_query<false> or_q;
+        count = or_q(make_cursors(index, query), index.num_docs());
+      }
+    } else if (tokens[0] == "TOP_10") {
+      if (intersection) {
+        ranked_and_query ranked_and_q(topk);
+        ranked_and_q(make_scored_cursors(index, *scorer, query),
+                     index.num_docs());
+        topk.finalize();
+        count = 1;
+      } else {
+        block_max_wand_query block_max_wand_q(topk);
+        block_max_wand_q(
+            make_block_max_scored_cursors(index, wdata, *scorer, query),
+            index.num_docs());
+        topk.finalize();
+        count = 1;
+      }
+    } else if (tokens[0] == "TOP_10_COUNT") {
+      if (intersection) {
+        scored_and_query and_q;
+        count = and_q(make_scored_cursors(index, *scorer, query), index.num_docs()).size();
+      } else {
+        or_query<true> or_q;
+        count = or_q(make_cursors(index, query), index.num_docs());
+      }
+    } else {
+      std::cout << "UNSUPPORTED\n";
+    }
+    std::cout << count << "\n";
+  }
 }
