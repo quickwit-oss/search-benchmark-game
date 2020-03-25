4a861041828d5a657c21c4bec428d712d39922e8
Antonio Mallia <me@antoniomallia.it>
2020-03-25 19:23:41 +0100

    All unsupported

--- engines/pisa-0.8.0/tools/do_query.cpp
+++ engines/pisa-0.8.0/tools/do_query.cpp
@@ -1,9 +1,21 @@
+#include <iostream>
+
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
+#include <boost/algorithm/string.hpp>
 
 int main(int argc, char const *argv[])
 {
     spdlog::drop("");
     spdlog::set_default_logger(spdlog::stderr_color_mt(""));
+
+    std::string line;
+    while (std::getline(std::cin, line))
+    {
+        std::vector<std::string> tokens;
+        boost::split(tokens, line, boost::is_any_of("\t"));
+
+        std::cout << "UNSUPPORTED" << std::endl;
+    }
 
 }
