3ad1f46960fcb800240fc9b6388c967e38707143
Antonio Mallia <me@antoniomallia.it>
2020-03-25 23:26:51 +0100

    Added json

--- engines/pisa-0.8.1/tools/build_index.cpp
+++ engines/pisa-0.8.1/tools/build_index.cpp
@@ -1,9 +1,12 @@
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
+#include <nlohmann/json.hpp>
+
 int main(int argc, char const *argv[])
 {
     spdlog::drop("");
     spdlog::set_default_logger(spdlog::stderr_color_mt(""));
 
+    nlohmann::json do_nothing;
 }
